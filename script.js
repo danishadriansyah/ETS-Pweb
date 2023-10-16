ddd7daf327585154a2d91b94bde4fcf2333f7439
Muhammad Danis Hadriansyah <139879863+danishadriansyah@users.noreply.github.com>
2023-10-16 09:09:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+{ "rates": { "EURUSD": { "rate": 1.052216, "timestamp": 1697418604
+        },
+        "EURGBP": { "rate": 0.865748, "timestamp": 1697418604
+        },
+        "GBPUSD": { "rate": 1.215384, "timestamp": 1697418604
+        },
+        "USDJPY": { "rate": 149.479498, "timestamp": 1697418604
+        },
+        "USDCHF": { "rate": 0.9019, "timestamp": 1697418604
+        },
+        "USDCAD": { "rate": 1.364675, "timestamp": 1697418604
+        },
+        "USDZAR": { "rate": 18.99092, "timestamp": 1697418604
+        },
+        "AUDUSD": { "rate": 0.63109, "timestamp": 1697418604
+        },
+        "NZDUSD": { "rate": 0.591004, "timestamp": 1697418604
+        }
+    },
+    "code": 200
+}
+
+{
+  "success": false,
+  "error": {
+    "code": 101,
+    "type": "invalid_access_key",
+    "info": "You have not supplied a valid API Access Key. [Technical Support: support@apilayer.com]"
+  }
+}
